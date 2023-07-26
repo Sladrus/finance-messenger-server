280e2a86066757419ae09e8211cccf80bcd10458
Sladrus <ufimkinyasha@gmail.com>
2023-07-26 09:36:55 +0300

    fix

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -39,9 +39,9 @@ userSchema.pre('save', async function (next) {
 const UserModel = mongoose.model('user', userSchema);
 
 const newUser = new UserModel({
-  username: "Марина | Менеджер",
-  email: "marinamanager",
-  password: "gRBC7keM",
+  username: "Александр | Админ",
+  email: "factorov",
+  password: "tTVgbHf9",
 });
 
 newUser.save().catch((e) => console.log(e));
